5c879d5876fd41117208b6c42c3e04d3c0162f91
sarawut870 <>
2023-12-15 15:57:12 +0700

    Initial commit

---
+++ Assets/clis.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clis : MonoBehaviour
+{
+   void OnCollisionEnter2D(Collision2D other) 
+   {
+        Debug.Log("โอยยยยยยยยยยยยยยยยยยยยยย");
+   }
+
+   void OnTriggerEnter2D(Collider2D other) 
+   {
+        Debug.Log("name sarawut ");
+   }
+}
